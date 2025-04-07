2e0c1b324a902dbe19a7eeafc9f9fc18fc595879
Kevin-rm <kevinramarozatovo@gmail.com>
2025-04-07 18:40:46 +0300

    Update huffman.rs

--- src/huffman.rs
+++ src/huffman.rs
@@ -1,12 +1,34 @@
+struct Source {
+    symbol: String,
+    probability: f64
+}
+
 struct Node {
     symbol: Option<String>,
     probability: f64,
-    left: Option<Self>,
-    right: Option<Self>
+    left: Option<Box<Self>>,
+    right: Option<Box<Self>>,
 }
 
-fn encode() {
+impl Node {
+    fn from_source(source: &Source) -> Self {
+        Self {
+            symbol: Some(source.symbol.clone()),
+            probability: source.probability,
+            left: None,
+            right: None
+        }
+    }
 
+
+}
+
+fn encode(sources: &[Source]) -> Vec<u8> {
+    let result: Vec<u8> = Vec::new();
+
+
+
+    result
 }
 
 fn decode() {
